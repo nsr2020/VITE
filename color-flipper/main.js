e768dd72f05281b0d48921682020050cf7019e74
nsr2020 <146729141+nsr2020@users.noreply.github.com>
2023-11-22 19:44:49 +0100

    cambios

---
+++ color-flipper/main.js
@@ -0,0 +1,48 @@
+import './style.css'
+const COLOR_PALETTE = {
+  '#28262C': 'Raisin Black',
+  '#998FC7': 'Blue Bell',
+  '#D4C2FC': 'Lavender Blue',
+  '#F9F5FF': 'Magnolia',
+  '#14248A': 'Resolution Blue'
+}
+
+
+const addOptionsToColorPicker = () => {
+  const colorPickerSelect = document.querySelector("#color-picker");
+
+  Object.keys(COLOR_PALETTE).forEach((color) => {
+    const option = document.createElement("option");
+    option.value = color;
+    option.innerText = COLOR_PALETTE[color];
+
+    colorPickerSelect.append(option);
+  })
+}
+const addEventListenerToColorPicker = () => {
+  const colorPickerSelect = document.querySelector("#color-picker");
+  
+  colorPickerSelect.addEventListener("change", (event) => {
+
+    const newColor = event.target.value; 
+    const colorNameText = `${COLOR_PALETTE[newColor]} | ${newColor}` 
+    const colorName = document.querySelector("#color-name")
+    colorName.innerText = COLOR_PALETTE[newColor] ? colorNameText : "-";
+    //Almacenamos el código de color
+
+
+    document.body.style.backgroundColor = newColor;
+    //Le aplicamos el background color con el código de color seleccionado
+    
+    
+    
+    
+    
+  });
+  
+};
+
+addOptionsToColorPicker();
+addEventListenerToColorPicker();
+
+
